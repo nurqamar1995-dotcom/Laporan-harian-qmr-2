ed8caa895ab6461c54303009521ec05c10f54e71
nurqamar1995-dotcom <nurqamar1995@gmail.com>
2026-08-21 23:40:31 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker Dasar
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Pass-through
+});
