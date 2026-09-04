741ba2dc2c2beef7a6bd42645165b3681da9d133
nurqamar1995-dotcom <nurqamar1995@gmail.com>
2026-09-04 09:41:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH NAMA/VERSI CACHE INI SETIAP KALI KAMU MELAKUKAN UPDATE KODE
-const CACHE_NAME = 'laporan-toko-v3.2';
+const CACHE_NAME = 'laporan-toko-v3.3';
 
 const ASSETS_TO_CACHE = [
     './',
